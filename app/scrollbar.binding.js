de5c419325f2d9d445c96f8ab3b7a68a3a6530bb
Joshua Austill <jlaustill@gmail.com>
2016-08-31 09:54:18 -0600

    new scrollbar binding started

---
+++ app/scrollbar.binding.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Joshua.Austill on 8/31/2016.
+ */
+
+var ko = require("knockout");
+var $ = require("jquery");
+ko.bindingHandlers.scrolly = {
+    init: function (element) {
+        "use strict";
+        $(element).mCustomScrollbar({axis:"yx", theme:"minimal-dark"});
+    }
+};
